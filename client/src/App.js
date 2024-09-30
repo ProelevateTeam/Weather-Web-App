474ec3f1e0807767d4b6f5de3ca12d6718391981
Singupalli Kartik <skartik1706@gmail.com>
2024-09-30 21:55:24 +0530

    bg changed

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
+  
+
+
     <div className="main-content">
+    <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
       <h1>Gurugram, Haryana</h1>
       <h3>33°C</h3>
       <p>Excessive Heat</p>
     </div>
+    </>
   )
 }
 
